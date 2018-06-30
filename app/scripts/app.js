d5ecc43f2be28a5dc7d7f3745f98c0b0a4820bab
Vladimir Kochenov <kochenov.vl@gmail.com>
2018-06-30 11:04:19 +0300

    добавлены состояния для пустого каталога

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,11 +2,7 @@
 // import $ from 'jquery';
 // import mainMenu from './../blocks/main-menu/main-menu';
 import pageHeader from './../blocks/page-header/page-header';
-import accordion from './../blocks/accordion/accordion';
 import catalogFilters from './../blocks/catalog-filters/catalog-filters';
-
-
-
 
 // window.onload = () => {
 // 	svg4everybody();
