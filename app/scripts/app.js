4f9f1fd1409f5a979b20cb90b370efbebd6b34d9
Vladimir Kochenov <kochenov.vl@gmail.com>
2018-06-30 21:11:23 +0300

    подогнал pixel-perfect по макету

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,5 @@
 // import svg4everybody from 'svg4everybody';
 // import $ from 'jquery';
-// import mainMenu from './../blocks/main-menu/main-menu';
 import pageHeader from './../blocks/page-header/page-header';
 import catalogFilters from './../blocks/catalog-filters/catalog-filters';
 
